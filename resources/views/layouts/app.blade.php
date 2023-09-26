58f4129fdb32d570c2e206de10738c9963654d4c
Manuel Martinez <119700105+Manuel65X@users.noreply.github.com>
2023-09-26 11:35:40 -0500

    Nuevos Componentes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
+        <title>UTP Social</title>
         @vite('resources/css/app.css')
         @vite('resources/js/app.js')
     </head>
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('titulo')
-    Modifica
+    Editar Perfil
 @endsection
 
 @section('contenido')
